cfe2f2756763f76588b8ebf317b29fd186cf841b
Fred Mac <freddymcgroarty@gmail.com>
2019-06-02 20:20:29 +0100

    exercise two: methods

--- ExerciseTwo.java
+++ ExerciseTwo.java
@@ -1,16 +1,23 @@
 public class ExerciseTwo {
+
+    private static String printReceipt (){
+        String s;
+        s = "+------------------------+ \n"
+          + "|                        | \n"
+          + "|      CORNER STORE      | \n"
+          + "|                        | \n"
+          + "| 2015-03-29  04:38PM    | \n"
+          + "|                        | \n"
+          + "| Gallons:       10.870  | \n"
+          + "| Price/gallon: $ 2.089  | \n"
+          + "|                        | \n"
+          + "| Fuel total:  $ 22.71   | \n"
+          + "|                        | \n"
+          + "+------------------------+ \n";
+          return s;
+    }
+
     public static void main( String[] args ) {
-        System.out.println( "+------------------------+" );
-        System.out.println( "|                        |" );
-        System.out.println( "|      CORNER STORE      |" );
-        System.out.println( "|                        |" );
-        System.out.println( "| 2015-03-29  04:38PM    |" );
-        System.out.println( "|                        |" );
-        System.out.println( "| Gallons:       10.870  |" );
-        System.out.println( "| Price/gallon: $ 2.089  |" );
-        System.out.println( "|                        |" );
-        System.out.println( "| Fuel total:  $ 22.71   |" );
-        System.out.println( "|                        |" );
-        System.out.println( "+------------------------+" );
+        System.out.println(printReceipt());
     }
 }
