9c52c3820a06ea41cc69fa566e6064f80f3f738d
Fred Mac <freddymcgroarty@gmail.com>
2019-06-02 22:40:04 +0100

    exercise two: multiple classes and stuff fun

--- ExerciseTwo.java
+++ ExerciseTwo.java
@@ -1,6 +1,66 @@
+import java.util.Scanner;  // Import the Scanner class
+// import java.lang.reflect;  // inspect java elements
+// Import the HashMap class
+import java.util.HashMap;
 public class ExerciseTwo {
 
-    private static String printReceipt (){
+    public static void main( String[] args ) {
+
+        Scanner scannerInput = new Scanner(System.in);
+        CountryQuerier countryQuerier = new CountryQuerier();
+
+        System.out.println("You choose: ");
+        countryQuerier.optionsHash().forEach((key,value) -> System.out.println(key + ". " + value + "\n"));
+
+        String input = scannerInput.nextLine();
+
+        while (!countryQuerier.isValid(input)) {
+
+            System.out.println(
+                input + " is invalid. Please choose a number between"
+            );
+            System.out.println("...");
+
+            input = scannerInput.nextLine();
+
+        }
+
+
+        // ReceiptPrinter receiptPrinter = new ReceiptPrinter();
+        // System.out.println(receiptPrinter.perform());
+    }
+
+
+    // private String chooseCountry (Scanner scannerInput){
+
+
+    //     // String country = scannerInput.nextLine();
+
+
+
+
+    // }
+}
+
+class CountryQuerier {
+
+    public HashMap optionsHash() {
+        HashMap<String, String> regionsHash = new HashMap<String, String>();
+        regionsHash.put("US", "1");
+        regionsHash.put("UK", "2");
+        regionsHash.put("EU", "3");
+
+        return regionsHash;
+    }
+
+    public boolean isValid(String selection) {
+        return optionsHash().containsValue(selection);
+    }
+}
+
+class ReceiptPrinter {
+
+    public String perform (){
         String s;
         s = "+------------------------+ \n"
           + "|                        | \n"
@@ -15,9 +75,5 @@ public class ExerciseTwo {
           + "|                        | \n"
           + "+------------------------+ \n";
           return s;
-    }
-
-    public static void main( String[] args ) {
-        System.out.println(printReceipt());
     }
 }
