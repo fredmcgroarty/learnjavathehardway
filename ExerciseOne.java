378a6290a989af28351db5c87639dec7f8c01b88
Fred Mac <freddymcgroarty@gmail.com>
2019-06-02 17:19:07 +0100

    Exercise One: Args & Concatenation

--- ExerciseOne.java
+++ ExerciseOne.java
@@ -2,11 +2,12 @@ import java.util.Calendar;
 
 public class ExerciseOne {
   public static void main( String[] args ) {
-
-    System.out.println("Hello World");
-    System.out.println("Today's date is:");
     System.out.println(
-      Calendar.getInstance().getTime()
+      "Hello " + args[0]
+    );
+
+    System.out.println(
+      "Today's date is: " + Calendar.getInstance().getTime()
     );
   }
 }
